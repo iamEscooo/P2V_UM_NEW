68daaf9a562fa217baabc42a7db08768c8b06766
Kotz <Christoph.Kotz@omv.com>
2025-07-03 09:50:08 +0200

    final cleanup?

--- P2V_scripts/warn_IPS_restart.cmd
+++
@@ -1,8 +0,0 @@
-REM  send to all CITRIX users warning msg
-
-msg * /server:somvat422001.ww.omv.com /TIME:300 /v <"\\somvat202005\PPS_share\P2V_Script-setup(new)\central\config\startup_message\restart-msg.txt"
-msg * /server:somvat422003.ww.omv.com /TIME:300 /v <"\\somvat202005\PPS_share\P2V_Script-setup(new)\central\config\startup_message\restart-msg.txt"
-msg * /server:somvat422008.ww.omv.com /TIME:300 /v <"\\somvat202005\PPS_share\P2V_Script-setup(new)\central\config\startup_message\restart-msg.txt"
-msg * /server:somvat422009.ww.omv.com /TIME:300 /v <"\\somvat202005\PPS_share\P2V_Script-setup(new)\central\config\startup_message\restart-msg.txt"
-msg * /server:somvat422017.ww.omv.com /TIME:300 /v <"\\somvat202005\PPS_share\P2V_Script-setup(new)\central\config\startup_message\restart-msg.txt"
-msg * /server:somvat422018.ww.omv.com /TIME:300 /v <"\\somvat202005\PPS_share\P2V_Script-setup(new)\central\config\startup_message\restart-msg.txt"
